5b33691fcc51d993db7226cb6e993daf931f26ce
estebanreyna <62328216+estebanreyna@users.noreply.github.com>
2020-05-03 18:35:35 -0500

    Update odoo_install.sh

--- odoo_install.sh
+++ odoo_install.sh
@@ -75,10 +75,12 @@ sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
 # Install Dependencies
 #--------------------------------------------------
 echo -e "\n--- Installing Python 3 + pip3 --"
-sudo apt-get install git python3 python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libpng12-0 gdebi -y
+sudo apt-get install git python3.8 python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libpng12-0 gdebi -y
 
 echo -e "\n---- Install python packages/requirements ----"
-sudo -H pip3 install -r https://github.com/odoo/odoo/raw/${OE_VERSION}/requirements.txt
+sudo -H pip3 install -r https://raw.githubusercontent.com/estebanreyna/InstallScript/13.0/requirements.txt
+sudo pip3 install lxml==4.3.2 -y
+sudo pip3 install Pillow==6.1.0 -y
 
 echo -e "\n---- Installing nodeJS NPM and rtlcss for LTR support ----"
 sudo apt-get install nodejs npm -y
